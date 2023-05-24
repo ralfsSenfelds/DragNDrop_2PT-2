22dfa2937ae1153b2bfcf47feffb7923216d2b09
Ralfs <ralfsssenfelds@gmail.com>
2023-05-24 12:19:42 +0300

    Isākts Drag and Drop skripts

---
+++ Assets/Scripts/DragDropScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropScript : MonoBehaviour,
+	IBeginDragHandler, IDragHandler, IEndDragHandler {
+    //Uzglabās norādi uz Objektu skriptu
+    public Objekti objektuSkripts;
+    //Uzglabās norādi uz katra objekta CanvasGroup
+    private CanvasGroup kanvasGrupa;
+    private RectTransform velkObjektuRectTransf;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        kanvasGrupa = GetComponent<CanvasGroup>();
+        velkObjektuRectTransf= GetComponent<RectTransform>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+}
